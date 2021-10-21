6f06f245d7bfb6866454c4ff46f07abbdb6fdf06
johnmaxnew <forevermeticulous@gmail.com>
2021-10-21 23:55:09 +0300

    added views

--- api/views.py
+++ api/views.py
@@ -6,4 +6,10 @@ from django.http import JsonResponse
 
 @api_view(['GET'])
 def getRoutes(request):
-    return JsonResponse(' API', safe=False)
+    routes = [
+        {
+            "xyz": "xyz",
+            "abc": "abc"
+        }
+    ]
+    return Response(routes)
