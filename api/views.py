547c17422babbad6e8eb545a7a51ea3cb35e12c4
johnmaxnew <forevermeticulous@gmail.com>
2021-10-21 23:57:55 +0300

    added views

--- api/views.py
+++ api/views.py
@@ -8,8 +8,34 @@ from django.http import JsonResponse
 def getRoutes(request):
     routes = [
         {
-            "xyz": "xyz",
-            "abc": "abc"
-        }
+            'Endpoint': '/notes/',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns an array of notes'
+        },
+        {
+            'Endpoint': '/notes/id',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns a single note object'
+        },
+        {
+            'Endpoint': '/notes/create/',
+            'method': 'POST',
+            'body': {'body': ""},
+            'description': 'Creates new note with data sent in post request'
+        },
+        {
+            'Endpoint': '/notes/id/update/',
+            'method': 'PUT',
+            'body': {'body': ""},
+            'description': 'Creates an existing note with data sent in post request'
+        },
+        {
+            'Endpoint': '/notes/id/delete/',
+            'method': 'DELETE',
+            'body': None,
+            'description': 'Deletes and exiting note'
+        },
     ]
     return Response(routes)
