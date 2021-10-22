674227a8259a09363c38ce2c45e559ecd275160c
johnmaxnew <forevermeticulous@gmail.com>
2021-10-22 23:11:44 +0300

    added views

--- api/views.py
+++ api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.http import JsonResponse
-
+from .models import Note
 
 @api_view(['GET'])
 def getRoutes(request):
