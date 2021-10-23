1ed41ff932ccbdd94b77e992f03f4cc6ec0b99e7
johnmaxnew <forevermeticulous@gmail.com>
2021-10-23 23:05:54 +0300

    connected to frontend

--- crud/settings.py
+++ crud/settings.py
@@ -50,7 +50,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'rest_framework',
-    'corsheaders',
+    # 'corsheaders',
+    "corsheaders",
     'api',
 ]
 
@@ -141,3 +142,11 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS: True
+
+# CORS_ALLOWED_ORIGINS = [
+#     "http://127.0.0.1:8000"
+# ]
+
+CORS_ORIGIN_WHITELIST = [
+     'http://localhost:3000'
+]
