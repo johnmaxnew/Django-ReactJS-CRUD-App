69ec55faa00ba9552eda507414ebe5d7f1be4908
johnmaxnew <forevermeticulous@gmail.com>
2021-10-23 23:53:23 +0300

    connected to frontend

--- crud/settings.py
+++ crud/settings.py
@@ -143,10 +143,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS: True
 
-# CORS_ALLOWED_ORIGINS = [
-#     "http://127.0.0.1:8000"
-# ]
-
 CORS_ORIGIN_WHITELIST = [
      'http://localhost:3000'
 ]
