eba2e3dc97b7ba09ea3d5385fb9d9f23184d2f0b
johnmaxnew <forevermeticulous@gmail.com>
2021-10-27 00:17:15 +0300

    configured routes

--- crud/settings.py
+++ crud/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'frontend/build/start'
+    BASE_DIR / 'frontend/build/static'
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
